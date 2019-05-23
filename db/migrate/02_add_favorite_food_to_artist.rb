b7febaccb79829593bee43037608fb807cf8033e
Alison Golden <alisong779@yahoo.com>
2019-05-23 18:52:31 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artist.rb
+++ db/migrate/02_add_favorite_food_to_artist.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtist < ActiveRecord::Migration
   def change
     add_column :artists, :favorite_food, :string
   end
